c2c8ea8b0f69ddcea8140e2c6240a50434476a7c
Firass Ziedan <firasslive@gmail.com>
2018-01-29 05:30:56 +0200

    Edit slider and add slick slider

---
+++ web/sites/default/files/php/twig/5a6e23652dcec_ds-field-reset.html.twig_JFEh721UH5Jfq_eBY0cRQn1JW/JkwB_uIu4rukNKX5CoWCuhU8fPL9nS-L4hzOiq3JCg8.php
@@ -0,0 +1,125 @@
+<?php
+
+/* modules/contrib/ds/templates/ds-field-reset.html.twig */
+class __TwigTemplate_d62d7dda088d382d2b266f52d72cfc2b2b395bec5865e2ff817c6658a96a51df extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 15, "for" => 21);
+        $filters = array("clean_class" => 16);
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if', 'for'),
+                array('clean_class'),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 15
+        if ( !($context["label_hidden"] ?? null)) {
+            // line 16
+            echo "  <div class=\"field-label-";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, \Drupal\Component\Utility\Html::getClass($this->getAttribute(($context["element"] ?? null), "#label_display", array(), "array")), "html", null, true));
+            echo "\">";
+            // line 17
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["label"] ?? null), "html", null, true));
+            if (($context["show_colon"] ?? null)) {
+                echo ":";
+            }
+            // line 18
+            echo "</div>
+";
+        }
+        // line 20
+        echo "
+";
+        // line 21
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["items"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["item"]) {
+            // line 22
+            echo "  ";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->getAttribute($context["item"], "content", array()), "html", null, true));
+            echo "
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['item'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/contrib/ds/templates/ds-field-reset.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  65 => 22,  61 => 21,  58 => 20,  54 => 18,  49 => 17,  45 => 16,  43 => 15,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("{#
+/**
+ * @file
+ * Template to reset all HTML for the field.
+ *
+ * Available variables:
+ * - element: The field element.
+ * - label: The label of the field.
+ * - show_colon: TRUE if colon should be displayed after label.
+ * - items: List of all the field items. Each item contains:
+ *   - attributes: List of HTML attributes for each item.
+ *   - content: The field item's content.
+ */
+#}
+{% if not label_hidden %}
+  <div class=\"field-label-{{ element['#label_display']|clean_class }}\">
+    {{- label }}{% if show_colon %}:{% endif -%}
+  </div>
+{% endif %}
+
+{% for item in items %}
+  {{ item.content }}
+{% endfor %}
+", "modules/contrib/ds/templates/ds-field-reset.html.twig", "/var/www/html/drupal/web/modules/contrib/ds/templates/ds-field-reset.html.twig");
+    }
+}
